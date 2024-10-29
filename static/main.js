39025897b1c32d689dd6f854881c2aeeae9385d2
mario <estrellamariosantos@gmail.com>
2024-10-28 21:19:47 -0500

    clean slate

--- static/main.js
+++ static/main.js
@@ -1,125 +1,27 @@
-import * as THREE from 'three'
-import * as hey from './modules/three.js' 
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/Addons.js';
 
-const Direction = Object.freeze({
-    up: Symbol('up'),
-    down: Symbol('down'),
-    left: Symbol('left'),
-    right: Symbol('right'),
-})
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const game_params = Object.freeze({
-    player_bbox: {
-        width: 10,
-        height: 20
-    },
-    // Pixels per second
-    base_speed: 10
-})
-
-const game_state = {
-    player_pos: {
-        x: 400,
-        y: 400,
-        toString() {
-            return `x: ${this.x}, y: ${this.y}`;
-        }
-    },
-    current_direction: Direction.right,
-    // Used to get delta_time
-    last_timestamp: 0
-}
-
-addEventListener("DOMContentLoaded", () => {
-    // Find all children of debug info and adds them to an object
-    // with the key as the value itself and a function to update it with a string parameter
-    // as the value.
-    // HTML element reference is held thanks to closure.
-    const register_debug_values = ((html_elements) => {
-        let ret = {};
-        for (let v of html_elements) {
-            if (v.className === 'debug_value') {
-                ret = {...ret, [v.getAttribute('value')]: (val) => v.textContent = val};
-            } else {
-                ret = {...ret, ...register_debug_values(v.children)};
-            }
-        }
-        return ret;
-    });
-    const debug_values = register_debug_values(document.getElementById('debug_info').children);
-
-
-    // Setting up three (scene, camera, and renderer)
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1 , 1000);
-
-    const canvas = document.getElementById("main_window");
-    const renderer = new THREE.WebGLRenderer( {
-        canvas: canvas
-    });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    
-
-    // Set up web socket
- 
-    // Set up main loop
-    const main_loop = ((t) => {
-        const deltatime = (t - game_state.last_timestamp) / 1000.0;
-        const fps = 1 / deltatime;
-
-        // Get input
-        addEventListener('keydown', (e) => {
-            switch (e.key) {
-                case "w":
-                    game_state.current_direction = Direction.up;
-                    break;
-                case "s":
-                    game_state.current_direction = Direction.down;
-                    break;
-                case "a":
-                    game_state.current_direction = Direction.left;
-                    break;
-                case "d":
-                    game_state.current_direction = Direction.right;
-                    break;
-            }
-        })
-
-        // Update game state
-        {
-            var pos = game_state.player_pos;
-            switch (game_state.current_direction) {
-                case Direction.up:
-                    pos.y -= game_params.base_speed * deltatime;
-                    break;
-                case Direction.down:
-                    pos.y += game_params.base_speed * deltatime;
-                    break;
-                case Direction.left:
-                    pos.x -= game_params.base_speed * deltatime;
-                    break;
-                case Direction.right:
-                    pos.x += game_params.base_speed * deltatime;
-                    break;
-            }
-        }
-
-        // Draw game state
-        {
-            const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-            const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-            const cube = new THREE.Mesh( geometry, material );
-            scene.add( cube );
-            
-            camera.position.z = 5;
-        }
-
-        function animate() {
-            renderer.render( scene, camera );
-        }
-
-        renderer.setAnimationLoop( animate );
-    })
-
-    main_loop();
+const canvas = document.getElementById("main_window");
+const renderer = new THREE.WebGLRenderer({
+    canvas:canvas
 });
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
